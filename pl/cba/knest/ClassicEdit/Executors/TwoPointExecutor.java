16a7a7aa2c48beab44afff6bc4dbce7444c8fbfb
Szperak <maciej.98@op.pl>
2014-04-05 18:59:33 +0200

    Code migration

--- pl/cba/knest/ClassicEdit/Executors/TwoPointExecutor.java
+++ pl/cba/knest/ClassicEdit/Executors/TwoPointExecutor.java
@@ -1,9 +1,7 @@
 package pl.cba.knest.ClassicEdit.Executors;
 
-import org.bukkit.ChatColor;
 
 import pl.cba.knest.ClassicEdit.ExecutorException;
-import pl.cba.knest.ClassicEdit.Filling;
 import pl.cba.knest.ClassicEdit.Creations.CuboidCreation;
 import pl.cba.knest.ClassicEdit.Creations.TwoPointCreation;
 
@@ -14,17 +12,5 @@ public class TwoPointExecutor extends CreationExecutor {
 	
 	public void execute() throws ExecutorException{
 		super.execute();
-
-		if(params.size()==1){
-			try{
-				f = Filling.parse(params.get(0));
-			}catch(Exception e){
-				throw new ExecutorException(ChatColor.RED+"Bad block name");
-			}
-		}
-		if(f != null && f.getMaterial() == null){
-			throw new ExecutorException(ChatColor.RED+"Invalid block name or id");
-		}
-		
 	}
 }

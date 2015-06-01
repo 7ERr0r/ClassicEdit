6e939c1eb830e5bd2aaab83804be92107e193173
Szperak <maciej.98@op.pl>
2015-06-01 20:03:28 +0200

    added sphreoid calculation madness

--- src/main/java/pl/cba/knest/ClassicEdit/creation/SpheroidCreation.java
+++ src/main/java/pl/cba/knest/ClassicEdit/creation/SpheroidCreation.java
@@ -9,9 +9,9 @@ public class SpheroidCreation extends CuboidCreation {
 		int rx = x-minx;
 		int ry = y-miny;
 		int rz = z-minz;
-		double dx = (((double)(rx+0.5d)/((double)width))-0.5d)*2;
-		double dy = (((double)(ry+0.5d)/((double)height))-0.5d)*2;
-		double dz = (((double)(rz+0.5d)/((double)length))-0.5d)*2;
+		double dx = (2*rx+1d)/width-1;
+		double dy = (2*ry+1d)/height-1;
+		double dz = (2*rz+1d)/length-1;
 		double d = Math.pow(Math.abs(dx), 2)+Math.pow(Math.abs(dy), 2)+Math.pow(Math.abs(dz), 2);
 		return d<=1;
 	}
